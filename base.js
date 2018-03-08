164f3fc4658c00485ebdd4bd05f15a862d623382
Nathan Costea <nathanC@knowclassic.com>
2018-03-07 23:39:04 -0500

    Importing variables from a different module.

--- base.js
+++ base.js
@@ -1,3 +1,5 @@
-let projectId = 99;
+import {projectId} from 'module1.js';
+import {id} from "module1.js";
 
 console.log("Logging out a message from inside base.js!");
+console.log(`Project id is ${projectId} while the id is ${id}`);
