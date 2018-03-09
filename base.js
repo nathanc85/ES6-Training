4f33e8bbd20574c288be0b9aab7667ef12808740
Nathan Costea <nathanC@knowclassic.com>
2018-03-09 11:23:21 -0500

    Names exports in modules.

--- base.js
+++ base.js
@@ -1,4 +1,10 @@
 import {projectId as pid, id} from 'module1.js';
+import prj from 'module1.js';
+
+import {showFunction, updatedFunction} from 'module1.js';
+showFunction();
+updatedFunction();
+showFunction();
 
 console.log("Logging out a message from inside base.js!");
-console.log(`Project id is ${pid} while the id is ${id}`);
+console.log(`Project id is ${pid} while the id is ${id} and the project name is ${prj}`);
--- module1.js
+++ module1.js
@@ -1,2 +1,8 @@
 export let projectId = 99;
 export let id = '22';
+
+let projectName = 'Super Project';
+export default projectName;
+
+export function showFunction() { console.log('In original...'); }
+export function updatedFunction() {showFunction = function() {console.log('In updated...');};}
