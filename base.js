373bc2f4c46b490b4d7ccb3b0dd73370e470cd60
Nathan Costea <nathanC@knowclassic.com>
2018-03-09 12:15:48 -0500

    extends and super

--- base.js
+++ base.js
@@ -8,3 +8,38 @@ showFunction();
 
 console.log("Logging out a message from inside base.js!");
 console.log(`Project id is ${pid} while the id is ${id} and the project name is ${prj}`);
+
+class Task{
+    constructor() {
+        console.log('Inside the Task constructor.');
+    }
+    showId() {
+        console.log('Id is 48');
+    }
+    getId() {
+        return 48;
+    }
+    getCount() {
+        return 60;
+    }
+}
+let task = new Task;
+console.log(typeof Task);
+console.log(typeof task);
+
+class MoreThanASimpleTask extends Task {
+    constructor() {
+        super();
+        console.log('Inside the MoreThanASimpleTask constructor.')
+    }
+    getId() {
+        return 50;
+    }
+    getCount() {
+        return super.getCount() + 2;
+    }
+};
+let more = new MoreThanASimpleTask();
+
+console.log(more.getId());
+console.log(more.getCount());
