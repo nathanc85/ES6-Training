f3c7a825a7b28f03afd77c9724184e89b4098585
nathanc85 <cnlnat@yahoo.com>
2018-03-12 13:17:34 -0400

    Reflect.apply

--- chapter8.js
+++ chapter8.js
@@ -1,6 +1,21 @@
-class Restaurant {}
+class Restaurant {
+    constructor(name, place) {
+        console.log(`${name} loves to eat at ${place}`);
+    }
+}
 
 console.log(typeof Reflect);
 
-let r = Reflect.construct(Restaurant, []);
+let r = Reflect.construct(Restaurant, ['Nathan', 'Outback']);
 console.log(r instanceof Restaurant);
+
+class Student {
+    constructor () {
+        this.id = 33;
+    }
+    show() {
+        console.log(this.id);
+    }
+}
+
+Reflect.apply(Student.prototype.show, { id: 44 }, {});
