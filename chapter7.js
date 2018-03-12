9b82351da0897bb779e82a15eae89546b95821ce
nathanc85 <cnlnat@yahoo.com>
2018-03-11 23:52:22 -0400

    chapter7

---
+++ chapter7.js
@@ -0,0 +1,8 @@
+let employee1 = { name : 'Harry'};
+let employee2 = { name : 'Janet'};
+
+let employees = new Map();
+employees.set(employee1, "123");
+employees.set(employee2, 'ABC');
+
+console.log(employees);
